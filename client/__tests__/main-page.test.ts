86f32c29813e40a5d434edf5b211f65b3c752dee
magom001 <magom001@yandex-team.ru>
2020-06-13 15:51:25 +0300

    fixup! Added jest-image-snapshot, sample integration test

--- client/__tests__/main-page.test.ts
+++ client/__tests__/main-page.test.ts
@@ -3,7 +3,11 @@ import puppeteer from 'puppeteer';
 describe('Integration tests > MainPage', () => {
     let browser: puppeteer.Browser;
     beforeAll(async () => {
-        browser = await puppeteer.launch();
+        browser = await puppeteer.launch({
+            headless: true,
+            executablePath: process.env.CHROMIUM_PATH,
+            args: ['--no-sandbox'],
+        });
     });
 
     afterAll(async () => {
@@ -12,6 +16,10 @@ describe('Integration tests > MainPage', () => {
 
     it('should render main page with default data', async () => {
         const page = await browser.newPage();
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+        });
         await page.goto('http://localhost:9009');
         const image = await page.screenshot({
             fullPage: true,
